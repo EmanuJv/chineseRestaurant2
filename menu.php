565dfb8a3b90523afde472cab0273c13caf070a1
EmanuJv <emanujv@gmail.com>
2023-11-13 10:55:19 -0600

    datos bd borrados, hacer scraping nuevamente

---
+++ menu.php
@@ -0,0 +1,311 @@
+<?php
+    require_once './database.php';
+    // Reference: https://medoo.in/api/select
+    $items = $database->select("tb_dishes","*");
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Exo:wght@500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+
+<body>
+    <header>
+        <nav class="top-nav">
+            <a href="./index.html"><img class="logo" src="./imgs/logoOscuro.png" alt="Shipu Logo"></a>
+
+            <p class="nav-title">Shípǔ</p>
+
+            <!--mobile nav btn (camping extracted)-->
+
+            <input class="mobile-cb" type="checkbox">
+            <label class="mobile-btn">
+                <span>
+
+                </span>
+            </label>
+
+            <!--Navigation List-->
+            <ul class="nav-list">
+                <li><a class="nav-list-link" href="./history.html">USER HISTORY</a></li>
+                <li><a class="nav-list-link" href="./menu.html">MENU</a></li>
+                <li><a class="nav-list-link" href="./cart.html">CART</a></li>
+                <li><a class="nav-list-link" href="./register.html">SIGN UP</a></li>
+                <li><a class="nav-list-link" href="./login.html">LOGIN</a></li>
+            </ul>
+
+
+        </nav>
+    </header>
+
+    <main>
+
+
+        <div class="menu-bg-container">
+            <h2 class="menuText">MENU</h2>
+            <p class="phras-text">"Gastronomy is the art of using food to create happiness"</p>
+        </div>
+
+        <!--Appetizers-->
+
+        <div class="cta-menu-icon">0
+            <h2 class="menuTxtCategories">Appetizers</h2>
+        </div>
+
+        <div class="menu">
+
+        <?php
+
+foreach ($items as $item) {
+    if ($item["category"] == 1) {
+    echo "<div class='food-items'>";
+    echo "<img class='dish_image' src='./imgs/imgsSC/" . $item["dish_image"] . "' alt='" . $item["dish_name"] . "'>";
+    echo "<div class='details'>";
+    echo "<div class='details-sub'>";
+    echo "<h5>" . $item["dish_name"] . "</h5>";
+    echo "<h5 class='price'>$" . $item["price"] . "</h5>";
+    echo "</div>";
+    echo "<p>" . substr($item["description"], 0, 200) . "...</p>";
+    echo "<a class='order-now' href='dishInfo.php?id=" . $item["dish_id"] . "'>Order NOW!</a>";
+    echo "</div>";
+    echo "</div>";
+    }
+    }
+        ?>
+
+
+        </div>
+
+        <!--Appetizers-->
+
+        
+
+        <!--Main Dishes-->
+
+        <div class="cta-menu-icon">
+            <h2 class="menuTxtCategories">Main Dishes</h2>
+        </div>
+
+        <div class="menu">
+
+        <div class="menu">
+
+<?php
+
+foreach ($items as $item) {
+if ($item["category"] == 2) {
+echo "<div class='food-items'>";
+echo "<img class='dish_image' src='./imgs/imgsSC/" . $item["dish_image"] . "' alt='" . $item["dish_name"] . "'>";
+echo "<div class='details'>";
+echo "<div class='details-sub'>";
+echo "<h5>" . $item["dish_name"] . "</h5>";
+echo "<h5 class='price'>$" . $item["price"] . "</h5>";
+echo "</div>";
+echo "<p>" . substr($item["description"], 0, 200) . "...</p>";
+echo "<a class='order-now' href='dishInfo.php?id=" . $item["dish_id"] . "'>Order NOW!</a>";
+echo "</div>";
+echo "</div>";
+}
+}
+?>
+
+
+</div>
+
+        </div>
+        <!--Main Dishes-->
+
+        <!--Desserts-->
+
+        <div class="cta-menu-icon">
+            <h2 class="menuTxtCategories">Desserts</h2>
+        </div>
+        <div class="menu">
+
+        <?php
+
+foreach ($items as $item) {
+if ($item["category"] == 3) {
+echo "<div class='food-items'>";
+echo "<img class='dish_image' src='./imgs/imgsSC/" . $item["dish_image"] . "' alt='" . $item["dish_name"] . "'>";
+echo "<div class='details'>";
+echo "<div class='details-sub'>";
+echo "<h5>" . $item["dish_name"] . "</h5>";
+echo "<h5 class='price'>$" . $item["price"] . "</h5>";
+echo "</div>";
+echo "<p>" . substr($item["description"], 0, 200) . "...</p>";
+echo "<a class='order-now' href='dishInfo.php?id=" . $item["dish_id"] . "'>Order NOW!</a>";
+echo "</div>";
+echo "</div>";
+}
+}
+?>
+
+        </div>
+        <!--Desserts-->
+
+
+        <!--Beverages-->
+
+        <div class="cta-menu-icon">
+            <h2 class="menuTxtCategories">Beverages</h2>
+        </div>
+
+        <div class="menu">
+            <div class="food-items">
+                <img src="./imgs/menu/beverageOne.jpg">
+                <div class="details">
+                    <div class="details-sub">
+                        <h5>BBQ Sandwich</h5>
+                        <h5 class="price"> $10 </h5>
+                    </div>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus reiciendis nam non quia! Earum
+                        eveniet quia minus nemo.</p>
+                    <a href="./dishInfo.html">Order Now!</a>
+                </div>
+            </div>
+
+            <div class="food-items">
+                <img src="./imgs/menu/BeverageTwo.jpg">
+                <div class="details">
+                    <div class="details-sub">
+                        <h5>BBQ Sandwich</h5>
+                        <h5 class="price"> $10 </h5>
+                    </div>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus reiciendis nam non quia! Earum
+                        eveniet quia minus nemo.</p>
+                    <a href="./dishInfo.html">Order Now!</a>
+                </div>
+            </div>
+
+            <div class="food-items">
+                <img src="./imgs/menu/beverageThree.jpg">
+                <div class="details">
+                    <div class="details-sub">
+                        <h5>BBQ Sandwich</h5>
+                        <h5 class="price"> $10 </h5>
+                    </div>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus reiciendis nam non quia! Earum
+                        eveniet quia minus nemo.</p>
+                    <a href="./dishInfo.html">Order Now!</a>
+                </div>
+            </div>
+
+            <div class="food-items">
+                <img src="./imgs/menu/beverageFour.png">
+                <div class="details">
+                    <div class="details-sub">
+                        <h5>BBQ Sandwich</h5>
+                        <h5 class="price"> $10 </h5>
+                    </div>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus reiciendis nam non quia! Earum
+                        eveniet quia minus nemo.</p>
+                    <a href="./dishInfo.html">Order Now!</a>
+                </div>
+            </div>
+
+            <div class="food-items">
+                <img src="./imgs/menu/beverageFive.png">
+                <div class="details">
+                    <div class="details-sub">
+                        <h5>BBQ Sandwich</h5>
+                        <h5 class="price"> $10 </h5>
+                    </div>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus reiciendis nam non quia! Earum
+                        eveniet quia minus nemo.</p>
+                    <a href="./dishInfo.html">Order Now!</a>
+                </div>
+            </div>
+
+            <div class="food-items">
+                <img src="./imgs/menu/beverageSix.png">
+                <div class="details">
+                    <div class="details-sub">
+                        <h5>BBQ Sandwich</h5>
+                        <h5 class="price"> $10 </h5>
+                    </div>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus reiciendis nam non quia! Earum
+                        eveniet quia minus nemo.</p>
+                    <a href="./dishInfo.html">Order Now!</a>
+                </div>
+            </div>
+
+        </div>
+        <!--Beverages-->
+        <div class="img-animated-container">
+            <img class="img-animated" src="./imgs/menu/animatedPic.jpg" alt="Imagen Animada">
+            <p class="menu-decoration-text">
+                "Chinese food, with its ancient culinary traditions, is a magical journey that takes us through <br> an
+                exquisite symphony of flavors and textures, where the balance between sweet and salty, spicy and soft,
+                <br> becomes a dance on the palate that transports us to remote places and immerses us in the rich
+                history and culture of this wonderful nation, reminding <br> us that food is much more than sustenance;
+                it is a bridge that connects people, a form of artistic expression and a legacy that is passed <br> down
+                from generation to generation, celebrating diversity and creativity in every bite."
+            </p>
+        </div>
+
+
+    </main>
+
+
+    <footer class="footer-container">
+        <div class="footer-content">
+            <section>
+                <h3 class="footer-decoration">Shipú Online Restaurant</h3>
+                <p>Welcome to Shipú, the groundbreaking virtual Chinese restaurant that brings the flavors of
+                    China
+                    to your doorstep! At Shipú, we combine authentic Chinese cuisine with modern culinary
+                    techniques
+                    to craft an array of delectable dishes that will transport you to the heart of China </p>
+            </section>
+            <div class="footer-schedule">
+                <h3 class="footer-decoration">Schedule and Services</h3>
+                <h4>Sunday to Thursday from 12:00 p.m. to 10:00 p.m. | Friday and Saturday from 12:00 p.m. to 11:00 p.m
+                    <br> <br>
+                    .Wi Fi <br>
+
+                    .After Office <br>
+
+                    .Spicy - Gluten Free - Vegetarian <br>
+
+                    .A/C
+                </h4>
+            </div>
+            <div class="footer-links">
+                <section>
+                    <h3 class="footer-decoration">Follow us on:</h3>
+                    <div class="cta-app-container">
+                        <a href="https://www.instagram.com"><img class="instagram" src="./imgs/instagram.png"
+                                alt="instagram"></a>
+                        <a href="https://www.facebook.com"><img class="facebook" src="./imgs/facebook.png"
+                                alt="facebook"></a>
+                        <a href="https://www.twitter.com"><img class="twitter" src="./imgs/twitter.png"
+                                alt="twitter"></a>
+
+                    </div>
+
+                </section>
+            </div>
+
+        </div>
+        <section class="download-app">
+            <h3>Download our App</h3>
+            <div class="cta-app-container">
+                <a href="#"><img src="./imgs/app-store.png" alt="Our app from App Store"></a>
+                <a href="#"><img src="./imgs/google-play.png" alt="Our app from Google Play"></a>
+            </div>
+        </section>
+        <p class="footer-legal">&copy; 2023. All rights reserved.</p>
+    </footer>
+</body>
+
+</html>
--- scraping/index.php
+++ scraping/index.php
@@ -3,7 +3,7 @@
 
     include('simple_html_dom.php');
     //link
-    $link =     "https://www.allrecipes.com/recipes/1899/world-cuisine/asian/chinese/appetizers/";
+    $link =     "https://www.allrecipes.com/recipes/79/desserts/";
 
     $filenames = [];
     $menu_item_names = [];
@@ -14,35 +14,32 @@
 
     $items = file_get_html($link);
 
-    //save meals info and filenames for the images
+
     foreach ($items->find('.card--no-image') as $item){
 
-        //busca la clase del titulo y lo guarda en un array
+       
         $title = $item->find('.card__title-text');
 
-        //hace otro scraping con un file_get_html() usando el href del item
-        //esto entra al href del link del card, para entrar al platillo donde está toda la info
         $details = file_get_html($item->href);
 
-        //busca la clase de la descripcion y la guarda en un array
+       
         $description = $details->find('.article-subheading');
 
-        //busca la clase de las imagenes y las guarda en un array
+      
         $image = $details->find('.primary-image__image');
 
-        //image debe tener las imagenes suficientes para llenar el array con las imagenes que se necesitan
-        //si el contenido de image es mayor a 0, entonces se le asigna el src de esas imagenes
+  
         if(count($image) > 0){
             $image_urls[] = $image[0]->src;
         }else{
             $replace_img = $item->find('.universal-image__image');
             if(count($replace_img) > 0){
-                //{'data-src'} se utiliza para acceder a la propiedad "data-src" de un objeto o array en PHP.
+            
                 $image_urls[] = $replace_img[0]->{'data-src'};
             }
         }
 
-        //algo parecido a la imagen, solo que dandole formato con trim, strtolower, etc
+
         if(count($description) > 0){
             if($menu_items == 0) break;
 
@@ -71,8 +68,7 @@
         echo "<br>";
         echo rand (1*10, 70*10)/10;
         echo "<br>";
-        //$menu_items--;
-        //if($menu_items == 0) break;
+       
     }
 
     //get and download images
@@ -80,33 +76,13 @@
         file_put_contents("../imgs/imgsSC/".$image.".jpg", file_get_contents($image_urls[$index]));
      }
 
-    //insert info
-    //Reference: https://medoo.in/api/insert
-
-    /*$database->select("tb_dishes",[
-        "[>]tb_categories"=>["id_category" => "id_category"],
-        "[>]tb_group_categories"=>["id_cat_group" => "id_cat_group"]
-    ],[
-        "tb_dishes.dish_id",
-        "tb_dishes.dish_name",
-        "tb_dishes.dish_image",
-        "tb_dishes.featured",
-        "tb_dishes.description",
-        "tb_dishes.price",
-        "tb_categories.category_name",
-        "tb_categories.category_description",
-        "tb_group_categories.cat_group_name",
-        "tb_group_categories.cat_group_description",
-    ],[
-        //"dish_id"=>$_GET["id"]
-    ]);*/
 
     for ($i = 0; $i < 10; $i++) {
         $database->insert("tb_dishes", [
             "dish_name" => $menu_item_names[$i],
             "dish_image" => $filenames[$i] . ".jpg",
-            "featured" => 0,
-            "category" => 1,
+            "featured" => 1,
+            "category" => 3,
             "description" => $menu_item_descriptions[$i],
             "price" => rand(1 * 10, 70 * 10) / 10
         ]);
